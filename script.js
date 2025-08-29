b69da28ec3ecce1f6716346a83766ce793a8c360
sabrina-alif <sabrinaalif538@gmail.com>
2025-08-29 14:08:26 +0600

    added copy button functionalities

--- script.js
+++ script.js
@@ -33,3 +33,29 @@ for (const button of callButtons) {
 
   });
 }
+
+
+
+// copy icon
+// Get the copy count element in the navbar
+const copyCountEl = document.getElementById("copy-count");
+let copyCount = parseInt(copyCountEl.innerText);
+
+// Get all Copy buttons
+const copyButtons = document.getElementsByClassName("copy-btn");
+
+// Add click event to each Copy button
+for (const button of copyButtons) {
+  button.addEventListener("click", function() {
+    // Find the hotline number in the same card
+    const parentCard = button.parentElement.parentElement; // button -> div.mt-4 -> card div.bg-white
+    const hotlineNum = parentCard.getElementsByClassName("hotline-num")[0].innerText;
+
+    // Increment copy count in navbar
+    copyCount += 1;
+    copyCountEl.innerText = copyCount;
+
+    // Show alert
+    alert(`Hotline number ${hotlineNum} copied!`);
+  });
+}
